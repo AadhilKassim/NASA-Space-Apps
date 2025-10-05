a6b5e3a54e0b85c49d4431d67d587432df2fb592
Aadhil Kassim <aadhikassim@gmail.com>
2025-10-05 13:21:19 +0530

    Updated _app.js

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,6 @@
 import '../src/index.css';
 import 'leaflet/dist/leaflet.css';
-import { AsteroidProvider } from '../src/contexts/asteroidcontext';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <AsteroidProvider>
-      <Component {...pageProps} />
-    </AsteroidProvider>
-  );
+  return <Component {...pageProps} />;
 }
